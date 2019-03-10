f6218ba422ed90d98c83df0535c6985fe13e03b1
zyamoji <uniquequeue+github@gmail.com>
2019-03-10 19:39:04 +0900

    Add parameter check for "gameDate"

--- saveDataToDB.py
+++ saveDataToDB.py
@@ -1,11 +1,18 @@
 import getYahooBaseballInfo
 import argparse
+import sys
 
 # 試合日を引数で受け取る
-# TODO Type Check
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("gameDate", help="set game date. fomat YYYYMMDDNN NN shoud set from 01 to 06")
 args = arg_parser.parse_args()
+
+# gameDate parameter check
+# gameDate must be 10 digits
+if len(args.gameDate) != 10 or not (args.gameDate.isdecimal()):
+    print("Please check parameter. gameDate format is YYYYMMDDNN. all digits.")
+    sys.exit(1)
+
 
 # 試合日
 # game date
@@ -16,9 +23,6 @@ gameDate = args.gameDate # 試合の日付とその日の何試合目か
 # 保存するSQLite DB
 # sqlite3 database name
 saveDBName = "baseballResult.db"
-
-# TODO
-# parameter check
 
 # データ取得
 # fetch data
